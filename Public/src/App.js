d45b3ebcb0d9db5eded6ec7e318ae6648ff934e9
Anupam Anand <aanand20@iitk.ac.in>
2022-07-05 09:10:53 +0530

    update

--- Public/src/App.js
+++ Public/src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+export default function App() {
   return (
-    <div>
-      app
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
